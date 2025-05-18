ca9e0e693912d196e068aa5072b677629afc1ed8
Joshua Jara <81706838+xlOzono@users.noreply.github.com>
2025-05-18 13:40:20 -0400

    chore(ui): limpieza de home-search tras fusión con estructura de ui-home
    - Se fusionaron los cambios de `home-search` en `ui-home`
    - Se eliminó lógica y archivos de desarrollo redundantes de home-search
    - Ahora se utilizan los componentes compartidos de `ui-home` para mantener consistencia

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
-import { HomepageComponent } from './components/home/homepage/homepage.component';
-import { SearchbarComponent } from './components/home/searchbar/searchbar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardVueloComponent } from './components/card-vuelo/card-vuelo.component';
@@ -19,8 +17,6 @@ import { SearchBarActivityComponent } from './components/search-bar-activity/sea
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomepageComponent,
-    SearchbarComponent,
     NavbarComponent,
     AppComponent,
     HomeComponent,
