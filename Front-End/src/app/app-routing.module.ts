aa8249402ec8380ce24e9ab38e80ff08c92dcebf
jorgeitonn <jorge.caceres.silva@alumnos.uta.cl>
2025-06-17 19:17:42 -0400

    style(ui): aplicar bootstrap en lugar de css y reordenar carpetas
    se eliminaron los casi la totalida de estilos css de cada componente y se utilizó en su totalidad bootstrap.
    se reorganizaron los componentes en carpetas para mayor organización.

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReviewsComponent } from './components/reviews/reviews.component';
-import { ExperienceComponent } from './components/experience/experience.component';
+import { ExperienceComponent } from './components/visualizar-experiencia/experience/experience.component';
 
 const routes: Routes = [
   { path: '', component: ExperienceComponent },
--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ExpImagesComponent } from './components/exp-images/exp-images.component';
-import { ExpDescComponent } from './components/exp-desc/exp-desc.component';
-import { ExpDispComponent } from './components/exp-disp/exp-disp.component';
-import { ExpOtherComponent } from './components/exp-other/exp-other.component';
+import { ExpImagesComponent } from './components/visualizar-experiencia/exp-images/exp-images.component';
+import { ExpDescComponent } from './components/visualizar-experiencia/exp-desc/exp-desc.component';
+import { ExpDispComponent } from './components/visualizar-experiencia/exp-disp/exp-disp.component';
+import { ExpOtherComponent } from './components/visualizar-experiencia/exp-other/exp-other.component';
 import { ReviewsComponent } from './components/reviews/reviews.component';
-import { ExperienceComponent } from './components/experience/experience.component';
+import { ExperienceComponent } from './components/visualizar-experiencia/experience/experience.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +29,9 @@ import { ExperienceComponent } from './components/experience/experience.componen
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: 'LOCALE_ID', useValue: 'es' } 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
