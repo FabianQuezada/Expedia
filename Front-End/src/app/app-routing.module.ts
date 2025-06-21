c44b902a6e4c49feb487aeedc1e49413ac63b59c
Andrea Navia <andrea.naviauwu@gmail.com>
2025-06-21 04:03:31 -0400

    feat(ui): Segunda versión de la pestaña de reprogramación

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VisualizarExperienciaComponent } from './paginas/visualizar-experiencia/visualizar-experiencia.component';
+import { ContenidoPagoComponent } from './paginas/pago/contenido-pago/contenido-pago.component';
+import { AnuncioComponent } from './paginas/anuncio/anuncio.component';
+import { PerfilComponent } from './paginas/perfil/perfil.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { ContenidoReprogramacionComponent } from './paginas/reprogramacion/contenido-reprogramacion/contenido-reprogramacion.component'; // ✅ NUEVO
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: VisualizarExperienciaComponent },
+  { path: 'pago', component: ContenidoPagoComponent },
+  { path: 'anuncio', component: AnuncioComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'reprogramacion', component: ContenidoReprogramacionComponent } // ✅ CORREGIDO
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { VisualizarExperienciaComponent } from './paginas/visualizar-experiencia/visualizar-experiencia.component';
+import { AnuncioComponent } from './paginas/anuncio/anuncio.component';
+import { PerfilComponent } from './paginas/perfil/perfil.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { ContenidoPagoComponent } from './paginas/pago/contenido-pago/contenido-pago.component';
+import { MetodoPagoComponent } from './paginas/pago/metodo-pago/metodo-pago.component';
+import { ContenidoReprogramacionComponent } from './paginas/reprogramacion/contenido-reprogramacion/contenido-reprogramacion.component';
+import { MetodoPagoReprogramacionComponent } from './paginas/reprogramacion/metodo-pago-reprogramacion/metodo-pago-reprogramacion.component';
+import { NavbarComponent } from './paginas/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisualizarExperienciaComponent,
+    AnuncioComponent,
+    PerfilComponent,
+    LoginComponent,
+    ContenidoPagoComponent,
+    MetodoPagoComponent,
+    ContenidoReprogramacionComponent,
+    MetodoPagoReprogramacionComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
