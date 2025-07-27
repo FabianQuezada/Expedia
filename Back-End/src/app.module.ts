b459e5d956f7da9fc31262c7cc091807bc154786
Liliana <liliana.galvez.y@gmail.com>
2025-07-26 23:00:03 -0400

    Update app.module.ts

--- Back-End/src/app.module.ts
+++ Back-End/src/app.module.ts
@@ -31,7 +31,7 @@ import { ResenaModule } from './resena/resena.module';
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         entities: [__dirname + '/**/*.entity.{ts,js}'],
-        synchronize: true,
+        synchronize: false,
         autoLoadEntities: true,
       }),
       inject: [ConfigService],
